fb1a66f19afc03979a4a718ba4fba39780c47565
mathisa <mathissou01@gmail.com>
2023-10-06 15:31:27 +0200

    FourthCommit

--- unit-test/math.js
+++ unit-test/math.js
@@ -10,4 +10,29 @@ const multiplyArray = (numbers) => {
   return numbers.reduce((acc, curr) => acc * curr, 1);
 };
 
-module.exports = { multiplyArray };
+const transformUsers = (users) => {
+  if (!Array.isArray(users)) {
+    console.log("zizié");
+    throw "Users must be provided.";
+  }
+  if (users.length > 8) {
+    throw "Too many users provided.";
+  }
+  const keyOfUsers = Object.keys(users);
+  if (!keyOfUsers.includes("id") || !keyOfUsers.includes("body")) {
+    throw "Users must be provided.";
+  }
+
+  return users.map((user) => {
+    return {
+      id: user.id,
+      name: user.body.userName,
+      age: user.body.userAge,
+    };
+  });
+};
+
+module.exports = {
+  multiplyArray,
+  transformUsers,
+};
